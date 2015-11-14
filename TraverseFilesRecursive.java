3b45a1646503f4d8331de2725c1f97a8067ce2d6
Galya <alice.gospodinova@gmail.com>
2015-11-14 20:32:18 +0200

    Traverse Files and Directories and print

--- TraverseFilesRecursive.java
+++ TraverseFilesRecursive.java
@@ -3,15 +3,15 @@ import java.util.HashSet;
 
 public class TraverseFilesRecursive {
     public static void main(String[] args) {
-        String path = "D:\\SOFTUNI";
-        traverse(path);
+        String path = "D:\\SOFTUNI\\Agorithms\\out";
+        traverse(path,0);
     }
 
-    private static void traverse(String path) {
+    private static void traverse(String path, int indent) {
         File dir= new File(path);
         File[] files = dir.listFiles();
         HashSet<File> visitedFiles= new HashSet<>();
-
+        System.out.print(new String(new char[indent]).replace("\0", "--"));
         System.out.println("Directory:" + dir.getName());
 
         for (File file : files) {
@@ -21,9 +21,10 @@ public class TraverseFilesRecursive {
             visitedFiles.add(file);
 
             if (file.isDirectory()) {
-                traverse(file.getPath());
+                traverse(file.getPath(), indent+1);
             }
             else {
+                System.out.print(new String(new char[indent]).replace("\0","--"));
                 System.out.println("--"+file.getName());
             }
         }
